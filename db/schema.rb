a871bc715d51b63fa964a9bdee91177f66df9813
Chris Trembley <cmt2057@rit.edu>
2014-12-22 21:40:18 -0500

    Week 1 finished

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141210013949) do
+ActiveRecord::Schema.define(version: 20141217230831) do
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "comments", force: true do |t|
     t.integer  "post_id"
     t.integer  "user_id"
     t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "post_categories", force: true do |t|
+    t.integer  "post_id"
+    t.integer  "category_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end

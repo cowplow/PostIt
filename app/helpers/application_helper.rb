5070262d34143900c610c43e82647020c8639fec
Chris Trembley <cmt2057@rit.edu>
2014-12-29 18:45:28 -0500

    Lesson 2 finished

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,14 @@
 module ApplicationHelper
   #method to check if url starts_with?('http://'), if not prepend that to the front and return 
+  def fix_url(url)
+    if (url.starts_with?('http://') || url.starts_with?('https://'))
+      return url
+    else
+       "http://www.#{url}"
+    end
+  end
+
+  def fix_date(date)
+    date.strftime("%l:%M%P %Z on %D")
+  end
 end
